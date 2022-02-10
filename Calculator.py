5ae2e0a954f453b6a5880105ee6ad53615cd930a
rafeek <rafeekbcr918@gmail.com>
2022-02-10 12:34:02 +0530

    size, resizable properties and name for the window has been given

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
-window=Tk()
-
+window=Tk() #this will create the new window
+window.geometry("312x324") #this wil give the size of the window
+window.resizable(0,0) #this will prevent window from resizing
+window.title("Calculator") #this wioll give name to that window calculator
 
 
 window.mainloop()
